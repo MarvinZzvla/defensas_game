54620111ade1da234430d9320d77014283d01f78
Marvin <hazielzs@gmail.com>
2022-09-12 03:57:49 -0600

    Fisicas corregidas y nuevos personajes

--- Assets/Scripts/PlayerMove.cs
+++ Assets/Scripts/PlayerMove.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerMove : MonoBehaviour
 { 
-    bool isR, isL, isU,isOnGround;
+    bool isR, isL, isU,canJump;
+    bool isRight, isLeft, isUp;
+    public float forceUp = 1200f;
     Rigidbody2D rb;
+    Animator animator;
 
     // Start is called before the first frame update
     //Este es un cambio de prueba
     void Start()
     {
      rb = gameObject.GetComponent<Rigidbody2D>();
+     animator =  gameObject.GetComponent<Animator>();
+     canJump = false;
     }
 
     // Update is called once per frame
@@ -21,72 +26,114 @@ public class PlayerMove : MonoBehaviour
        Application.LoadLevel(0);
     }
 
+    void getInput(){
+         
+         if(Input.GetKey(KeyCode.UpArrow)){
+            isUp = true;
+        }
+        else if(Input.GetKey(KeyCode.LeftArrow)){
+            isLeft = true;
+            isRight = false;
+            isUp = false;
+        }
+        else if(Input.GetKey(KeyCode.RightArrow)){
+            isRight = true;
+            isUp = false;
+            isLeft = false;
+        }
+    
+        else{
+            isUp = false;
+            isLeft = false;
+            isRight = false;
+        }
+        playAnimation();
+    }
+
+    void playAnimation(){
+        //Run Movemment
+        if(isUp || isU){
+            animator.SetBool("isJump",true);
+            animator.SetBool("isRun",false);
+        }
+        else if(isLeft || isL){
+            animator.SetBool("isRun",true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            animator.SetBool("isJump",false);
+        }
+        else if(isRight || isR){
+            animator.SetBool("isRun",true);
+            gameObject.GetComponent<SpriteRenderer>().flipX = false;
+            animator.SetBool("isJump",false);
+        }
+        else{
+            animator.SetBool("isRun",false);
+            animator.SetBool("isJump",false);      
+        }
+    
+    }
+
     void Update()
     {
-
+        getInput();
       
 
-        if(isR){
+        if(isR || isRight){
             rb.AddForce(new Vector2(2000f * Time.deltaTime,0));
             }
-        if(isL){
+        else if(isL || isLeft){
             rb.AddForce(new Vector2(-2000f * Time.deltaTime,0));
-            }
-        
+            } 
 
     }
 
     void FixedUpdate(){
-        if(isU && isOnGround){
-            rb.AddForce(new Vector2(0,1000));
-        }
+        if((isUp || isU) && canJump){
+            rb.AddForce(new Vector2(0,forceUp));
+            canJump = false;
+            
+        } 
     }
         
     
-    void OnTriggerEnter2D(Collider2D collider){
-        if(collider.name == "ground"){
-            gameObject.GetComponent<Animator>().SetBool("isFall",false);
-            isOnGround = true;
+    void OnCollisionEnter2D(Collision2D collider){
+        print("Suelo");
+        if(collider.collider.name == "ground"){
+            canJump = true;
+            animator.SetBool("isFall",false);
             }
     }
 
-    void OnTriggerExit2D(Collider2D collider)
-    {
-        if(collider.name == "ground"){
-            gameObject.GetComponent<Animator>().SetBool("isFall",true);
-            gameObject.GetComponent<Animator>().SetBool("isRun",false); 
-            gameObject.GetComponent<Animator>().SetBool("isJump",false);
-            isOnGround = false;
-            isU = false;
-            }
+    void OnCollisionExit2D(Collision2D coll){
+        if(coll.collider.name == "ground"){
+        animator.SetBool("isFall",true);
+        }
     }
+
 
     
 
    public void buttonLeft(){
     isL = true;
-    gameObject.GetComponent<Animator>().SetBool("isRun",true);
-    gameObject.GetComponent<SpriteRenderer>().flipX = true;
+    
     } 
     public void buttonLeftRelease(){
-        isL = false;
-        gameObject.GetComponent<Animator>().SetBool("isRun",false);
+     isL = false;
         }
     public void buttonRight(){
         isR = true;
-        gameObject.GetComponent<Animator>().SetBool("isRun",true);
-        gameObject.GetComponent<SpriteRenderer>().flipX = false;
+        
         } 
     public void buttonRightRelease(){
         isR = false;
-        gameObject.GetComponent<Animator>().SetBool("isRun",false);
+        
         }
     public void buttonUp(){
         isU = true;
-        gameObject.GetComponent<Animator>().SetBool("isJump",true);
+        
         } 
         public void buttonUpRelease(){
-            
+            isU = false;
         }
     
 }
